bccee05158d9948316e6ef683e5dd5d809034866
Iulia <BBK-PiJ-2016-27@users.noreply.github.com>
2017-11-16 14:11:41 +0000

    Update NumberPersonalities.scala

--- NumberPersonalities.scala
+++ NumberPersonalities.scala
@@ -2,81 +2,73 @@ object NumberPersonalities {
 
   val limit: Int = 100
 
-  def main (args: Array [String]) {}
+  def main(args: Array[String]) {}
 
-    def isPrime(n: Int): Boolean = {
-      var divisorCount: Int = 0
-      for (i<-1 to n) {
-        if (n % i == 0)
-          divisorCount += divisorCount
-          println (divisorCount.toString)
+  def isPrime(n: Int): Boolean = (2 until n) forall (x => n % x != 0)
 
-      }
-      (divisorCount == 2)
-    }
-   // isPrime(8)
-    //isPrime(11)
-    //isPrime (7)
-
-    def isPrime2(n:Int) : Boolean = (2 until n) forall (x => n % x != 0)
-
-    isPrime2(8)
-    isPrime2(7)
-    isPrime2(51)
-    isPrime2(5)
+  isPrime(8)
+  isPrime(7)
+  isPrime(51)
+  isPrime(5)
 
 
-   def squared (x:Int) = {x*x}
+  def squared(x: Int) = {
+    x * x
+  }
 
-   def presentInSequence (x:Int): Boolean = {
-    val repeatingSequence = Array (4,16,37,58,89,145,42,20)
+  def presentInSequence(x: Int): Boolean = {
+    val repeatingSequence = Array(4, 16, 37, 58, 89, 145, 42, 20)
     repeatingSequence.contains(x)
   }
 
-   def sumOfDigits (x:Int):Int = {
-    var copy:Int = x
-    var sum, iterations: Int = 0
-    val numberLength = x.toString.length
-    while (iterations < numberLength) {
-      sum+=squared(copy%10)
-      copy/=10
-      iterations+=1
-    }
-    sum
+
+  def getIntList(x: Int): List[Int] = {
+    x.toString.map(_.asDigit).toList
   }
 
-   def isHappy(n: Int): Boolean = {
-     if (presentInSequence(n)) false
-     else if (n != 1) {
-       isHappy(sumOfDigits(n))
-     } else true
-   }
-     sumOfDigits(1024)
-     sumOfDigits(1111)
-     sumOfDigits(222)
-     sumOfDigits(7)
-     sumOfDigits(19)
-
-     isHappy(1)
-     isHappy(10000)
-     isHappy(19)
-     isHappy(22)
-     isHappy(89)
-     isHappy(58)
+  def squaredSumOfDigits(list: List[Int]) = {
+    list.map(x => x * x).sum
+  }
 
 
-    var triangleHelper = 0
-    def isTriangular(n: Int): Boolean = {
-      isTriangularHelp(n, 1)
-    }
+  def sumOfDigitsHelper(i: Int) = {
+    squaredSumOfDigits(getIntList(i))
+  }
 
-      def isTriangularHelp (x: Int,y:Int) : Boolean = {
-        if (x<0) false
-        else if (x - y ==0) true
-        else isTriangularHelp(x-y, y+1)
-    }
+
+  def isHappy(n: Int): Boolean = {
+    if (presentInSequence(n)) false
+    else if (n != 1) {
+      isHappy(sumOfDigitsHelper(n))
+    } else true
+  }
+  
+  def isTriangular(n: Int): Boolean = {
+    isTriangularHelp(n, 1)
+  }
+
+  def isTriangularHelp(x: Int, y: Int): Boolean = {
+    if (x < 0) false
+    else if (x - y == 0) true
+    else isTriangularHelp(x - y, y + 1)
+  }
 
   println("****")
+  
+  sumOfDigitsHelper(1024)
+  sumOfDigitsHelper(1111)
+  sumOfDigitsHelper(222)
+  sumOfDigitsHelper(7)
+  sumOfDigitsHelper(19)
+
+  isHappy(1)
+  isHappy(10000)
+  isHappy(19)
+  isHappy(22)
+  isHappy(89)
+  isHappy(58)
+  isHappy(145)
+
   isTriangular(3)
   isTriangular(15)
   isTriangular(10)
@@ -85,14 +77,13 @@ object NumberPersonalities {
   isTriangular(6)
   isTriangular(7)
 
-
   //isSquare(n: Int): Boolean
 
   //isSmug(n: Int): Boolean
 
   //isHonest(n: Int): Boolean
 
-  //isPronic(n: Int): Boolean
+  //isPronic
 
   //isDeficient(n: Int): Boolean
 
@@ -101,4 +92,4 @@ object NumberPersonalities {
   //isAbundant(n: Int): Boolean
 
   //sumOfPositiveDivisorsOf(n: Int): Int
-
+}
